1d10c0240b413fe2f2d79d0d5247223ed82ee4e6
BobbyHon <bryanthuynh35@yahoo.com>
2024-05-20 15:41:38 -0700

    Testing out API

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const object = {
+    'hammy':{
+        'age': 40,
+        'ability': 'stuff',
+        'cost': 10, 
+    },
+    'toby':{
+        'age': 27,
+        'ability': 'other stuff',
+        'cost': 14,
+    },
+    'blank':{
+        'age': 0,
+        'ability': 'blank',
+        'cost': 0,
+    },
+}
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/api', (req, res) =>{
+    res.json(object)
+})
+app.get('/api/:name', (req, res) =>{
+    const gameName = req.params.name.toLowerCase()
+    if (object[gameName])
+        {
+            res.json(object[gameName])
+        }
+    else{
+        res.json(object['blank'])
+    }
+})
+app.listen(PORT, () =>{
+    console.log(`Running on port ${PORT}`)
+})
